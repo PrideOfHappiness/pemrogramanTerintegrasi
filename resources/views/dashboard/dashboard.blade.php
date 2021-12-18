b9e58b0d09e826ae1532bc1bf2df66873840c690
Renaldi <soeryadi.renaldi@gmail.com>
2021-12-18 20:30:59 +0700

    First Commit

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,43 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid container">
+  @yield('dashboard')
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</div>
+    @yield('logout')
+</nav>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          @yield('slidebar')
+        </ul>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        @yield('akun')
+        <br><br>
+      </div>
+    </nav>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        @yield('layout')
+      </div>
+      @yield('isi')
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
+    </body>
+</html>
+
---
+++ resources/views/user/loginUser.blade.php
@@ -0,0 +1,43 @@
+@extends('dashboard\navbar')
+
+@section('isiNavbar')
+<body>
+    <div class="auth">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-7 col-lg-5">
+                    <div class="card mb-3 border-dark">
+                        <div class="card-head bg-dark" align="center">
+                            <h3 class="text-center mb-3"> SI TUNTAS </h3>
+                        </div>
+                        <div class="card-body">
+                            <h3 class="text-center mb-3">Pengguna Lainnya</h3>
+                            <form action="/Admin/Home" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" name="user_id" class="form-control mb-3" id="user_id" placeholder="NIK" autofocus required>
+                                        @error('user_id')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="password" class="form-control mb-3" id="passMhs" name="password" placeholder="Password" required>
+                                </div>
+                                <input type="hidden" id="status" name="status" value="Pejabat">
+                                <div class="form-group form-check mb-3">
+                                    <input type="checkbox" class="form-check-input" id="rmbMe">
+                                    <label class="form-check-label">Remember me</label>
+                                </div>
+                                <button type="submit" class="w-100 btn btn-primary btn-lg">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+@endsection
